3d1ed2ed5052a3949c5e587957912346a436f2d7
BHARATH0153 <bharath0153@github.com>
2026-05-20 03:54:12 +0000

    [fix] Remove ValidatedModelSerializer workaround from OrganizationGeoSettingsSerializer #633

--- openwisp_controller/geo/api/serializers.py
+++ openwisp_controller/geo/api/serializers.py
@@ -1,5 +1,3 @@
-from copy import copy
-
 from django.contrib.humanize.templatetags.humanize import ordinal
 from django.core.exceptions import ValidationError
 from django.db import models, transaction
@@ -30,18 +28,6 @@ class OrganizationGeoSettingsSerializer(ValidatedModelSerializer):
         model = OrganizationGeoSettings
         fields = "__all__"
         read_only_fields = ["organization"]
-
-    # Workaround for https://github.com/openwisp/openwisp-utils/issues/633
-    # TODO: Remove when the Bug is fixed in openwisp-utils
-    def validate(self, data):
-        Model = self.Meta.model
-        instance = copy(self.instance) if self.instance else Model()
-        for key, value in data.items():
-            # avoid direct assignment for m2m (not allowed)
-            if not isinstance(Model._meta.get_field(key), models.ManyToManyField):
-                setattr(instance, key, value)
-        instance.full_clean(exclude=self.exclude_validation)
-        return data
 
 
 class LocationDeviceSerializer(ValidatedModelSerializer):
